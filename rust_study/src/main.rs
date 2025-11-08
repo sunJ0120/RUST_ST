fe31f906d00821283731b794a3272e7de314aef3
sunJ0120 <sspure1214@gmail.com>
2025-11-08 12:20:50 +0900

    feat: 3.1. 변수와 가변성 학습 완료

--- rust_study/src/main.rs
+++ rust_study/src/main.rs
@@ -1,36 +1,7 @@
-use std::cmp::Ordering;
-use std::io;
-use rand::Rng;
+mod example2;
+mod example3;
 
 fn main() {
-    println!("내가 무슨 번호를 생각했게~");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("너가 생각한 번호를 알려줘봐");
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("값을 읽는데 실패했어");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("유효한 숫자를 입력해줘!");
-                continue;
-            }
-        };
-
-        println!("너가 생각한 번호 : {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("너가 생각한 번호가 너무 작아!"),
-            Ordering::Greater => println!("너가 생각한 번호가 너무 커!"),
-            Ordering::Equal => {
-                println!("너가 생각한 번호가 맞아! 축하해!");
-                break;
-            }
-        }
-    }
+    // example2::run();
+    example3::run();
 }
