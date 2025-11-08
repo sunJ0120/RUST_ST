98a1585257f141fedadbaab272ab3ae85b6cb20e
sunJ0120 <sspure1214@gmail.com>
2025-11-08 10:43:12 +0900

    feat: 2-2. 비밀번호 생성 완료

--- rust_study/src/main.rs
+++ rust_study/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("내가 무슨 번호를 생각했게~");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("(비밀 번호: {secret_number})");
+
+    println!("너가 생각한 번호를 알려줘봐");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("값을 읽는데 실패했어");
+
+    println!("너가 생각한 번호 : {guess}");
 }
