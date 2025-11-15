c0c9c5556df10b6b9896b240e4570df9000f9a04
sunJ0120 <sspure1214@gmail.com>
2025-11-15 21:10:01 +0900

    feat: SET, GET 명령어 행위 구현 및 TEST 완료
    1. SET 구현 완료 -> 반환은 String
    2. GET 구현 완료 -> 반환은 Optional
    3. test 구현 완료

--- rudis/src/store.rs
+++ rudis/src/store.rs
@@ -11,4 +11,43 @@ impl Store{
             data: Arc::new(Mutex::new(HashMap::new())),
         }
     }
+
+    // SET 명령어
+    pub fn set(&self, key: &str, value: &str) -> String {
+        let mut data = self.data
+            .lock()
+            .expect("🦀 락을 얻는데 실패하였습니다.");
+        data.insert(key.to_string(), value.to_string());
+
+        "OK".to_string()
+    }
+
+    // GET 명령어
+    pub fn get(&self, key: &str) -> Option<String> {
+        let data = self.data
+            .lock()
+            .expect("🦀 락을 얻는데 실패하였습니다.");
+
+        data.get(key).cloned()
+    }
+}
+
+// 테스트
+#[cfg(test)]
+mod tests {
+    use super::Store;
+
+    #[test]
+    fn test_set(){
+        let store = Store::new();
+        store.set("key", "rudis");
+        assert_eq!(store.get("key"), Some("rudis".to_string()));
+    }
+
+    #[test]
+    fn test_get_nonexistent_key(){
+        let store = Store::new();
+        let result = store.get("not_exist");
+        assert_eq!(result, None);
+    }
 }
