1f846c69232c8adb40b752d040a1b242f823b3d7
sunJ0120 <sspure1214@gmail.com>
2025-11-16 21:02:11 +0900

    feat: 열거형에 CLI 연결을 위한 정의 완료

--- rudis/src/command.rs
+++ rudis/src/command.rs
@@ -5,4 +5,47 @@ pub enum Command {
     Del { key: String },
     Expire { key: String, seconds: i64 },
     Ttl { key: String },
+    Exit,
+    Unknown,
+}
+
+impl Command {
+    pub fn parse(input: &str) -> Result<Command, String> {
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+
+        match parts.as_slice() {
+            ["SET", key, value] => Ok(Command::Set{
+                key: key.to_string(),
+                value: value.to_string(),
+            }),
+
+            ["GET", key] => Ok(Command::Get {
+                key: key.to_string(),
+            }),
+
+            ["DEL", key] => Ok(Command::Del {
+                key: key.to_string(),
+            }),
+
+            ["EXPIRE", key, seconds] => {
+                match seconds.parse::<i64>() {
+                    Ok(sec) => Ok(Command::Expire {
+                        key: key.to_string(),
+                        seconds: sec,
+                    }),
+                    Err(_) => Err("올바르지 않은 TTL 값 입니다.".to_string()),
+                }
+            }
+
+            ["TTL", key] => Ok(Command::Ttl {
+                key: key.to_string(),
+            }),
+
+            ["EXIT"] | ["exit"] | ["QUIT"] | ["quit"] => Ok(Command::Exit),
+
+            [] => Err("빈 명령어 입니다.".to_string()),
+
+            _ => Ok(Command::Unknown),
+        }
+    }
 }
