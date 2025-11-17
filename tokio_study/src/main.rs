da57ae312af8586b6d0e8a8a26cbd7f76234d9ec
sunJ0120 <sspure1214@gmail.com>
2025-11-17 23:44:12 +0900

    feat: Tokio 스터디 >> 서버 소켓 커넥션 작성하기

--- tokio_study/src/main.rs
+++ tokio_study/src/main.rs
@@ -1,8 +1,10 @@
 mod async_ex;
 mod mini_redis_server_ex_1;
+mod spawning;
 
 #[tokio::main]
 async fn main() {
     // mini_redis_server_ex_1::run().unwrap();
-    async_ex::run().await; //future만 반환하는 lazy execution이라 .await 필수
+    // async_ex::run().await; //future만 반환하는 lazy execution이라 .await 필수
+    spawning::run().await;
 }
