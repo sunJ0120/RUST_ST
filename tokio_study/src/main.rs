7cf047db50dd01bcbb06fecc610b3150a77769b2
sunJ0120 <sspure1214@gmail.com>
2025-11-17 21:47:24 +0900

    feat: Tokio 스터디_async 비동기 방식과 멀티 스레딩 활용하기

---
+++ tokio_study/src/main.rs
@@ -0,0 +1,8 @@
+mod async_ex;
+mod mini_redis_server_ex_1;
+
+#[tokio::main]
+async fn main() {
+    // mini_redis_server_ex_1::run().unwrap();
+    async_ex::run().await; //future만 반환하는 lazy execution이라 .await 필수
+}
